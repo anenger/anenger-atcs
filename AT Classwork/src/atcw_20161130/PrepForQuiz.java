193e10dd2b537d51f9c9165f20c40802ab9a5736
anenger <anenger@192.168.108.124>
2016-11-30 11:08:12 -0500

    Second commit

---
+++ AT Classwork/src/atcw_20161130/PrepForQuiz.java
@@ -0,0 +1,14 @@
+package atcw_20161130;
+
+public class PrepForQuiz {
+	static int times2(int x){
+		return x*2;
+	}
+	public static void main(String[] args){
+		Quiz q1 = new Quiz(10, 3);
+		q1.lostLife();
+		System.out.println(q1.getLives());
+		q1.setScore(times2(q1.getScore()));
+		System.out.println(q1.getScore());
+	}
+}
---
+++ AT Classwork/src/atcw_20161130/Quiz.java
@@ -0,0 +1,25 @@
+package atcw_20161130;
+
+public class Quiz {
+	int score;
+	int lives;
+	Quiz(int s, int l){
+		score = s; 
+		lives = l;
+	}
+	int getScore(){
+		return score;
+	}
+	void setScore(int s){
+		score = s;
+	}
+	int getLives(){
+		return lives;
+	}
+	void setLives(int l){
+		lives = l;
+	}
+	void lostLife(){
+		lives = lives - 1;
+	}
+}
