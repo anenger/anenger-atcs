97c7623ef2dbbc3ead02f709f8e7c2d312429826
anenger <anenger@usanenger.home>
2016-12-14 23:30:29 -0500

    Final version of betterarray with all necessary elements for the project

--- Andrew Enger's Projects/src/betterarray_20161201/Array.java
+++ Andrew Enger's Projects/src/betterarray_20161201/Array.java
@@ -6,7 +6,11 @@ public class Array {
 	Array(int l){
 		mainArray = new int[l];
 	}
+	Array(){
+		mainArray = new int[0];
+	}
 
+	//adds "size" slots to the array, copies all old values over
 	void resize(int size){
 		int[] copyArray = new int[mainArray.length + size];
 		for (int i = 0; i < mainArray.length; i++){
@@ -17,78 +21,98 @@ public class Array {
 		mainArray = copyArray;
 	}
 
+	//adds one spot to the end of the array and fills it with the inputted value
 	void push(int value){
 		resize(1);
 		mainArray[mainArray.length - 1] = value;
 	}
 
+	//removes the last value from the array, resizes the array, and returns the value
 	int pop(){
 		int value = mainArray[mainArray.length - 1];
 		resize(-1);
 		return value;
 	}
 
+	//inserts the "value" at the "index", moves all other values up which therefore resizes the array +1
 	void insert(int index, int value){
 		shiftValues(1, index);
 		mainArray[index] = value;
 	}
 
+	//removes a value from the array and deletes the index of the value, shifts all values above it down
 	void remove(int index){
 		mainArray[index] = 0;
 		shiftValues(-1, index);
 		resize(-1);
 	}
 	
+	//updates a specific value in the array, must specify an index to change, does not shift any other values
 	void update(int index, int value){
 		mainArray[index] = value;
 	}
 	
+	//swaps two values at indices "i1" and "i2"
 	void swap(int i1, int i2){
 		int value = mainArray[i1];
 		mainArray[i1] = mainArray[i2];
 		mainArray[i2] = value;
 	}
 	
+	//returns the maximum value in the array, starts at the first value and checks if any are greater than it
 	int max(){
-		int max = mainArray[0];
-		for (int x = 0; x < mainArray.length; x++){
-			if (mainArray[x] > max){
-				max = mainArray[x];
-			}
+		if (mainArray.length > 0){
+			int max = mainArray[0];
+				for (int x = 0; x < mainArray.length; x++){
+					if (mainArray[x] > max){
+						max = mainArray[x];
+					}
+				}
+			return max;
 		}
-		return max;
+		else{
+			return 0;
+		}
 	}
 	
+	//returns the minimum value in the array, starts at the first value and checks if any are smaller than it
 	int min(){
-		int min = mainArray[0];
-		for (int x = 0; x < mainArray.length; x++){
-			if (mainArray[x] < min){
-				min = mainArray[x];
+		if (mainArray.length > 0){
+			int min = mainArray[0];
+			for (int x = 0; x < mainArray.length; x++){
+				if (mainArray[x] < min){
+					min = mainArray[x];
+				}
 			}
-		}
 		return min;
+		}
+		else{
+			return 0;
+		}
 	}
 	
-	int average(){
-		int average = 0;
-		int sum = 0;
-		for (int x = 0; x < mainArray.length; x++){
-			sum += mainArray[x];
-		}
+	//takes the average of the array by adding all values and then dividing by the length (number of values)
+	//double to return a more accurate average
+	double average(){
+		double average = 0;
+		double sum = this.sum();
 		average = sum / mainArray.length;
 		return average;
 	}
 	
+	//returns the size of the array
 	int size(){
-		int size = mainArray.length;
-		return size;
+		return mainArray.length;
 	}
 
-	int getValue(int index){
+	//returns a value at the "index"
+	int get(int index){
 		return mainArray[index];
 	}
 	
-	void sort(){
+	//sorts using the bubblesort algorithm
+	//prints the amount of time it took in ms
+	void bubblesort(){
 		Date d1 = new Date();
 		for (int x = mainArray.length - 1; x > 0; x--){
 			for (int y = 0; y < x; y++){
@@ -101,6 +125,8 @@ public class Array {
 		System.out.println(d2.getTime() - d1.getTime() + " milliseconds to complete bubble sort.");
 	}
 	
+	//sorts using the insertionsort algorithm
+	//prints the amount of time it took in ms
 	void insertionsort(){
 		Date d1 = new Date();
 		for (int x = 0; x < mainArray.length; x++){
@@ -114,6 +140,7 @@ public class Array {
 		System.out.println(d2.getTime() - d1.getTime() + " milliseconds to complete insertion sort.");
 	}
 	
+	//takes the sum of all values of the array, used in average
 	int sum(){
 		int sum = 0;
 		for (int x = 0; x < mainArray.length; x++){
@@ -121,7 +148,7 @@ public class Array {
 		}
 		return sum;
 	}
-
+	//checks if the specific index is in the array
 	boolean checkIndex(int index){
 		if (index < mainArray.length && index >= 0){
 			return true;
@@ -132,6 +159,7 @@ public class Array {
 		}
 	}
 	
+	//fills the array with random values
 	void fill(int min, int max){
 		int random = 0;
 		int range = (max - min) + 1;
@@ -141,6 +169,15 @@ public class Array {
 		}
 	}
 	
+	//fills the array with values "counter" apart, e.g. 0,2,4,6 or 0,3,6,9
+	void fill(int counter){
+		for (int x = 0; x < mainArray.length; x++){
+			mainArray[x] = x*counter;
+			
+		}
+	}
+	
+	//shifts the values by 1 starting at the first place
 	void shiftValues(){
 		resize(1);
 		for (int x = 0; x < mainArray.length; x++){
@@ -148,22 +185,21 @@ public class Array {
 		}
 	}
 	
+	//shifts the values by "shift" starting at "index"
+	//overload of shiftValues
 	void shiftValues(int shift, int index){
 		resize(shift);
-		for (int x = mainArray.length - 1; x >= index; x--){
-			if (x == 0){
-				mainArray[x] = mainArray[x + 1];
-			}
-			else{
+		for (int x = mainArray.length - 1; x > index; x--){
 				mainArray[x] = mainArray[x - shift];
-			}
 		}
 	}
 	
+	//returns the array as an array
 	int[] getArray(){
 		return mainArray;
 	}
 
+	//returns the array as a string of comma-seperated values
 	public String toString(){
 		String arrayString = "";
 		for (int x = 0; x < mainArray.length; x++){
--- Andrew Enger's Projects/src/betterarray_20161201/BetterArray.java
+++ Andrew Enger's Projects/src/betterarray_20161201/BetterArray.java
@@ -1,18 +1,122 @@
 package betterarray_20161201;
+import java.util.Scanner;
+//this system could be used to keep a database of users such as in a big company or otherwise
+//it would be very easy when inputting user data to store it in this array because of push, pop, sort, etc
+//specifically, I am using this betterarray to keep track of student id numbers
+//I can store values in specific slots, add a value anywhere in the array, shift all other values up,
+//in this case I would probably have two arrays, one to store the students name and one to store their id number
+//the reason there are a lot of input.nextLine(); is because the old values from the scanner were getting stuck in the scanner, and that was the only way i could think of to clear them out
 
 public class BetterArray {
+	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args){
-		Array a1 = new Array(10000);
-		a1.fill(-100,100);
-		a1.insertionsort();
-		Array a2 = new Array(10000);
-		a2.fill(-100,100);
-		a2.sort();
-		System.out.println(a1.toString());
-		a1.insert(0, 999);
-		System.out.println(a1.toString());
-		System.out.println(a1.average());
-		System.out.println(a1.max());
-		System.out.println(a1.min());
-	}
+		//creating a betterarray with the student id number
+		Array a1 = new Array();
+		boolean loop = true;
+		while (loop){
+			System.out.println("Choose an option: ");
+			System.out.println("1: Add new student");
+			System.out.println("2: Edit a student");
+			System.out.println("3. Delete a student");
+			System.out.println("4. Fill array with random student ID numbers");
+			System.out.println("5. Get system data");
+			System.out.println("6. Close program");
+			int switchint = input.nextInt();
+			input.nextLine();
+			switch(switchint){
+			case 1:
+				System.out.println("Please enter the new student's ID number: ");
+				System.out.println("ID Number: ");
+				int id = input.nextInt();
+				input.nextLine();
+				a1.push(id);
+				System.out.println("ID stored.");
+				System.out.println("");
+			break;
+			case 2:
+				System.out.println("Current students in the system are: ");
+				System.out.println(a1);
+				System.out.println("Enter the index of the student you would like to edit: ");
+				int i = input.nextInt();
+				input.nextLine();
+				if (a1.checkIndex(i) == true){
+					System.out.println(a1.get(i) + " is the ID value you have chosen. Please enter the updated ID value: ");
+					int val = input.nextInt();
+					input.nextLine();
+					a1.update(i, val);
+					System.out.println("The value at place " + i + " is now " + val);
+				}
+				System.out.println("");
+			break;
+			case 3:
+				System.out.println("Current students in the system are: ");
+				System.out.println(a1);
+				System.out.println("Please input the index of the ID value you would like to delete: ");
+				int i2 = input.nextInt();
+				input.nextLine();
+				if (a1.checkIndex(i2) == true){
+					System.out.println(a1.get(i2) + " is the ID value you have chosen. Are you sure you want to delete this value?");
+					System.out.println("Y or N");
+					String check = input.nextLine();
+					input.nextLine();
+						if (check.equals("Y")){
+							a1.remove(i2);
+						}
+						else{
+							System.out.println("Student kept.");
+						}
+				}
+				System.out.println("");
+			break;
+			case 4:
+				System.out.println("Pick two values between 0 and infinity: ");
+				System.out.println("1: ");
+				int v1 = input.nextInt();
+				input.nextLine();
+				System.out.println("2: ");
+				int v2 = input.nextInt();
+				input.nextLine();
+				System.out.println("Choose the new length of the array: ");
+				int l = input.nextInt();
+				input.nextLine();
+				a1.resize(l - a1.size());
+				a1.fill(v1, v2);
+				System.out.println("Array filled.");
+			break;
+			case 5:
+				System.out.println(a1);
+				System.out.println("The minimum ID number is: " + a1.min());
+				System.out.println("The maximum ID number is: " + a1.max());
+				System.out.println("Would you like to sort the ID values?");
+				System.out.println("Y or N");
+				String c1 = input.nextLine();
+				if (c1.equals("Y")){
+					a1.insertionsort();
+				}
+				else{
+					System.out.println("Values kept");
+				}
+			break;
+			case 6:
+				loop = false;
+			break;
+			}
+		}
+	//showing further capabilities of the betterarray
+		Array a2 = new Array(200);
+		System.out.println(a2);
+		a2.fill(2);
+		System.out.println(a2);
+		a2.fill(-20, 20);
+		System.out.println(a2);
+		a2.bubblesort();
+		System.out.println(a2);
+		a2.fill(-20, 20);
+		System.out.println(a2);
+		a2.insertionsort();
+		System.out.println(a2);
+		System.out.println(a2.average());
+		System.out.println(a2.max());
+		System.out.println(a2.min());
+}
 }
