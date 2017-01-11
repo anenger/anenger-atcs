2b4d41ef18e68b612cbec6e6ffbce345f94b5fe2
anenger <anenger@192.168.109.107>
2017-01-11 09:45:36 -0500

    Filearray

--- Andrew Enger's Projects/src/filearray_20170111/FileArray.java
+++ Andrew Enger's Projects/src/filearray_20170111/FileArray.java
@@ -2,26 +2,49 @@ package filearray_20170111;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class FileArray {
-	FileArray(){
-		int[] array = new int[0];
+	private int[] mainArray;
+	FileArray(int l){
+		mainArray = new int[l];
 	}
-	static void readFile(String filename){
+	void readFile(String filename){
 		FileReader fr;
 		try{
 			fr = new FileReader(filename);
 			BufferedReader br = new BufferedReader(fr);
 			String input;
 			while((input = br.readLine()) != null){
-				System.out.println(input);
+				input += input;
 			}
+			String[] sa = input.split(",");
+			int[] data = new int[sa.length];
+			for(int x = 0; x < sa.length; x++){
+				data[x] = Integer.parseInt(sa[x]);
+			}
+			mainArray = data;
 			br.close();
 		}catch(Exception e){
 			
 		}
 	}
-	static void writeFile(){
-		
+	void writeFile(String filename){
+		try {
+			FileWriter fw = new FileWriter(filename);
+			for (int i = 0; i < mainArray.length; i++){
+				fw.write(mainArray[i] + "\n");
+			}
+			fw.close();
+		}catch(Exception e){
+			
+		}
+	}
+	void enterArray(int[] a){
+		writeFile("out.txt");
+	}
+	int[] returnArray(){
+		return mainArray;
 	}
 }
--- Andrew Enger's Projects/src/filearray_20170111/Main.java
+++ Andrew Enger's Projects/src/filearray_20170111/Main.java
@@ -3,8 +3,7 @@ package filearray_20170111;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
