833e68963a6b857572ae68238300af8ee698b016
anenger <anenger@packer.edu>
2017-02-28 09:47:49 -0500

    Quiz first commit

---
+++ Andrew Enger's Projects/src/testgui_20170228/QuizGUI.java
@@ -0,0 +1,25 @@
+package testgui_20170228;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class QuizGUI extends Frame implements ActionListener {
+	
+	private Panel startPa1;
+	private Panel startPa2;
+	private Panel startPa3;
+	private Panel mainPa1;
+	private Panel mainPa2;
+	private Panel mainPa3;
+	private Panel endPa1;
+	private Panel endPa2;
+	private Panel endPa3;
+
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent evt){
+		
+	}
+}
