c7c8d3cd09e3c458b89d273033652bedbe54219c
Andrew Enger <anenger@packer.edu>
2017-09-24 21:53:17 -0400

    natasha

--- Andrew Enger's Projects/src/natasha_20170924/NatashaHelp.java
+++ Andrew Enger's Projects/src/natasha_20170924/NatashaHelp.java
@@ -2,7 +2,7 @@ package natasha_20170924;
 
 public class NatashaHelp {
 	public static void main(String[] args){
-		int y = 1904;
+		int y = 1805;
 		boolean isLeapYear = (y%4 == 0 && y%100 != 0) || (y%100 == 0 && y%400 == 0);
 		System.out.println(isLeapYear);
 	}
