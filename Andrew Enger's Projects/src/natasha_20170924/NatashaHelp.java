e095f655523716839c4b92264d0722294c57a17a
Andrew Enger <anenger@packer.edu>
2017-11-14 22:47:28 -0500

    test

--- Andrew Enger's Projects/src/natasha_20170924/NatashaHelp.java
+++ Andrew Enger's Projects/src/natasha_20170924/NatashaHelp.java
@@ -1,10 +1,20 @@
 package natasha_20170924;
 
+import java.util.Scanner;
 public class NatashaHelp {
 	public static void main(String[] args){
-		int y = 1805;
-		boolean isLeapYear = (y%4 == 0 && y%100 != 0) || (y%100 == 0 && y%400 == 0);
-		System.out.println(isLeapYear);
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please input the number of variables you would like to store:");
+		int vars = input.nextInt();
+		int i = 0;
+		int total = 0;
+		while (i < vars){
+			System.out.println("Enter a value:");
+			int temp = input.nextInt();
+			total = total + temp;
+			i++;
+		}
+		System.out.println((total / vars) + " is your average.");
 	}
 
 }
